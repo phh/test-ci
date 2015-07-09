55096de8c182fd3a995ec45b12b1b0a7dc76dfe6
Patrick <phh@peytz.dk>
2015-07-09 22:44:18 +0200

    Update phpcs.sh

--- bin/phpcs.sh
+++ bin/phpcs.sh
@@ -6,7 +6,5 @@ phpenv rehash
 
 # Install Coding Standards
 
-phpcs -p -s -v -n .
-
 # git clone https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git wordpress-coding-standards
 # git clone https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git wordpress-coding-standards/CodeSniffer/Standards/Peytz-WordPress
