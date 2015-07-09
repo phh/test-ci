566a8bb738183d82e7b181aa4337aabbc75dac07
Patrick <phh@peytz.dk>
2015-07-09 23:02:36 +0200

    Update pre-phpcs.sh

--- bin/pre-phpcs.sh
+++ bin/pre-phpcs.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 # Install PHP Codesniffer
-pyrus install pear/PHP_CodeSniffer
-phpenv rehash
+#pyrus install pear/PHP_CodeSniffer
+#phpenv rehash
+
+composer install
 
 # Install Coding Standards
-composer create-project wp-coding-standards/wpcs:dev-master --no-dev
+#composer create-project wp-coding-standards/wpcs:dev-master --no-dev
 
 # git clone https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git wordpress-coding-standards
 # git clone https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git wordpress-coding-standards/CodeSniffer/Standards/Peytz-WordPress
